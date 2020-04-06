0d0cf64d0a91541373b4671f9e0de6b5437784fb
DONADEI Emrick <emrick.donadei@hotmail.fr>
2020-04-06 19:33:32 +0200

    prepare keepalived container, still need to test it

--- keepalived/master.sh
+++ keepalived/master.sh
@@ -1,3 +1,2 @@
-$HAS_FLOATING_IP=192.168.0.30     #Adresse IP globale aux 2 LB
-echo "HAS_FLOTING_IP = "$HAS_FLOATING_IP"
-
+HAS_FLOATING_IP='192.168.0.30'     #Adresse IP globale aux 2 LB
+echo $HAS_FLOATING_IP
